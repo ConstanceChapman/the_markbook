e8848db68b085aaf0529889a50098dd155809971
Constance Chapman <constancealicechapman224@gmail.com>
2018-08-21 17:15:17 +0200

    changed routes so that lesson path has teaching set param

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :teaching_sets, only: [:index] do
-    resources :lessons, only: [:index]
+    resources :lessons, only: [:index, :show]
     resources :tasks, only: [:index, :create]
     resources :report_cycles, only: :index
   end
-  resources :lessons, only: :show
   resources :scores, only: :update
   resources :tasks, only: [:update]
   resources :marks, only: [:create, :update]
