c106d749d4940c3829c5922aadff888a2d464840
Saskia Menke <menkesaskia@web.de>
2018-08-22 11:08:22 +0200

    add destroy method to task and include in routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,11 @@ class TasksController < ApplicationController
     redirect_to teaching_set_tasks_path(@task.teaching_set)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
   private
 
   def task_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   resources :teaching_sets, only: [:index] do
     resources :lessons, only: [:index, :show]
     resources :tasks, only: [:index, :create]
-    resources :report_cycles, only: :index
+    resources :report_cycles, only: [:index]
   end
-  resources :scores, only: :update
-  resources :tasks, only: [:update]
+  resources :scores, only: [:update]
+  resources :tasks, only: [:update, :destroy]
   resources :marks, only: [:create, :update]
   resources :reports, only: [:update]
 end
