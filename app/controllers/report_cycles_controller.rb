fc269e453f23a5d687e29a145f7e48c933ba50b4
Constance Chapman <constancealicechapman224@gmail.com>
2018-08-22 11:33:28 +0200

    view and update reports in reports_cycle index

--- app/controllers/report_cycles_controller.rb
+++ app/controllers/report_cycles_controller.rb
@@ -1,4 +1,5 @@
 class ReportCyclesController < ApplicationController
   def index
+    @set_pupils = TeachingSet.find(params[:teaching_set_id]).set_pupils
   end
 end
--- app/models/set_pupil.rb
+++ app/models/set_pupil.rb
@@ -1,6 +1,7 @@
 class SetPupil < ApplicationRecord
   belongs_to :teaching_set
   belongs_to :pupil
+  has_many :report_cycles, through: :teaching_set
   has_many :scores
   has_many :marks
   has_many :reports
