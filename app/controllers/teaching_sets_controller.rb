8a60e40558c54655b4c1996e290c97f9f20e1c49
Saskia Menke <menkesaskia@web.de>
2018-08-23 15:10:27 +0200

    work in progress on calendar

--- app/controllers/teaching_sets_controller.rb
+++ app/controllers/teaching_sets_controller.rb
@@ -1,5 +1,43 @@
 class TeachingSetsController < ApplicationController
   def index
     @no_tabbar = true
+
+    @lessons = []
+    current_user.teaching_sets.each do |ts|
+      ts.lessons.each do |l|
+        @lessons << l
+      end
+    end
+
+    @events = @lessons.map do |l|
+      {
+        title: "#{l.teaching_set.year_group}, #{l.teaching_set.subject}",
+        start: "#{l.start_time}",
+        allDay: false
+      }
+    end
   end
 end
+
+# array, hash
+#   array, hash
+
+# eventSources: [
+#   {
+#     events: [
+#       {
+#         title  : 'event1',
+#         start  : '2010-01-01'
+#       },
+#       {
+#         title  : 'event2',
+#         start  : '2010-01-05',
+#         end    : '2010-01-07'
+#       },
+#       {
+#         title  : 'event3',
+#         start  : '2010-01-09T12:30:00',
+#       }
+#     ],
+#   }
+# ]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,34 @@
 import "bootstrap";
+import "fullcalendar";
+import "fullcalendar/dist/fullcalendar.min.css"
 import { toggleTabClass } from "../components/tabs.js"
+
+// $(document).ready(function(){
+
+
+
+//   console.log(events)
+// });
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+  var calendar = $('#calendar')
+  calendar.fullCalendar({
+  //   events: this.dataset.events
+  })
+
+  // console.log(calendar.dataset.events);
+});
+
+
+// var calendar = $('#calendar')
+
+// $( document ).ready(function() {
+//   calendar.fullCalendar({
+//     events: [JSON.parse(calendar.dataset.events)]
+//   })
+// });
+
+// calendar.fullCalendar({
+// })
