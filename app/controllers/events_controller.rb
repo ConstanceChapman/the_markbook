e7d8e00ac07858c28035b29bcaf71a0a47430af9
Saskia Menke <menkesaskia@web.de>
2018-08-24 17:23:05 +0200

    create calendar-list-view on lesson-index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,4 +19,22 @@ class EventsController < ApplicationController
       }
     end
   end
+
+  def filtered_index
+    # @lessons_ts = []
+    @teaching_set = TeachingSet.find(params[:teaching_set_id])
+    # @teaching_set.lessons.each do |l|
+    #   l
+    # end
+
+    @events_ts = @teaching_set.lessons.map do |l|
+      {
+        title: "#{l.teaching_set.year_group}, #{l.teaching_set.subject}",
+        start: "#{l.start_time.strftime('%Y-%m-%dT%H:%M:%S')}",
+        end: "#{l.end_time.strftime('%Y-%m-%dT%H:%M:%S')}",
+        color: "#{l.teaching_set.color}",
+        url: "teaching_sets/#{l.teaching_set.id}/lessons/#{l.id}"
+      }
+    end
+  end
 end
