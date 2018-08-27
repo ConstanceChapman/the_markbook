3ee46350e8f3ace646fb809c6c01fa58848ad479
Constance Chapman <constancealicechapman224@gmail.com>
2018-08-27 12:57:54 +0200

    mark comments added and marks styled

--- app/controllers/report_cycles_controller.rb
+++ app/controllers/report_cycles_controller.rb
@@ -3,7 +3,7 @@ class ReportCyclesController < ApplicationController
     # @articles = ArticleDecorator.decorate_collection(Article.all)
     teaching_set = TeachingSet.find(params[:teaching_set_id])
     # @set_pupils = SetPupilDecorator.decorate_collection(teaching_set.set_pupils)
-    @set_pupils = teaching_set.set_pupils
+    @set_pupils = teaching_set.set_pupils.includes([:reports, :scores, :marks, :pupil])
   end
 
   private
