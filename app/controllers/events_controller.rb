d2a66d9b99367c10beea3e49e6f43cba47dc8962
Saskia Menke <menkesaskia@web.de>
2018-08-31 11:10:17 +0200

    no empty comments shown on calendar list view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,7 @@ class EventsController < ApplicationController
 
     @events_ts = @teaching_set.lessons.map do |l|
       {
-        title: "#{l.teaching_set.year_group}, #{l.teaching_set.subject} #{('(' + l.comment + ')') if l.comment}",
+        title: "#{l.teaching_set.year_group}, #{l.teaching_set.subject} #{('(' + l.comment + ')') if l.comment && !l.comment.empty?}",
         start: "#{l.start_time.strftime('%Y-%m-%dT%H:%M:%S')}",
         end: "#{l.end_time.strftime('%Y-%m-%dT%H:%M:%S')}",
         color: "#{l.teaching_set.color}",
